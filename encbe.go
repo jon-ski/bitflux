009e9ab86d2047ad5ee0f296f90558c471fcc022
jskinner <jonathan@jskinner.info>
2025-10-22 10:59:27 -0600

    added BE encoder

---
+++ encbe.go
@@ -0,0 +1,122 @@
+package bitflux
+
+import (
+	"encoding"
+	"io"
+	"math"
+)
+
+type EncBE struct {
+	W   io.Writer
+	N   int64
+	Err error
+}
+
+func NewEncBE(w io.Writer) *EncBE { return &EncBE{W: w} }
+
+func (e *EncBE) push(p []byte) {
+	if e.Err != nil || len(p) == 0 {
+		return
+	}
+	for off := 0; off < len(p); {
+		n, err := e.W.Write(p[off:])
+		e.N += int64(n)
+		off += n
+		if err != nil {
+			e.Err = err
+			return
+		}
+		if n == 0 { // defensive: writer made no progress
+			e.Err = io.ErrShortWrite
+			return
+		}
+	}
+}
+
+func (e *EncBE) U8(v uint8) {
+	var b [1]byte
+	b[0] = byte(v)
+	e.push(b[:])
+}
+
+func (e *EncBE) U16(v uint16) {
+	var b [2]byte
+	b[1] = byte(v)
+	b[0] = byte(v >> 8)
+	e.push(b[:])
+}
+
+func (e *EncBE) U32(v uint32) {
+	var b [4]byte
+	b[3] = byte(v)
+	b[2] = byte(v >> 8)
+	b[1] = byte(v >> 16)
+	b[0] = byte(v >> 24)
+	e.push(b[:])
+}
+
+func (e *EncBE) U64(v uint64) {
+	var b [8]byte
+	b[7] = byte(v)
+	b[6] = byte(v >> 8)
+	b[5] = byte(v >> 16)
+	b[4] = byte(v >> 24)
+	b[3] = byte(v >> 32)
+	b[2] = byte(v >> 40)
+	b[1] = byte(v >> 48)
+	b[0] = byte(v >> 56)
+	e.push(b[:])
+}
+
+func (e *EncBE) I8(v int8) {
+	e.U8(uint8(v))
+}
+
+func (e *EncBE) I16(v int16) {
+	e.U16(uint16(v))
+}
+
+func (e *EncBE) I32(v int32) {
+	e.U32(uint32(v))
+}
+
+func (e *EncBE) I64(v int64) {
+	e.U64(uint64(v))
+}
+
+func (e *EncBE) F32(v float32) {
+	e.U32(math.Float32bits(v))
+}
+
+func (e *EncBE) F64(v float64) {
+	e.U64(math.Float64bits(v))
+}
+
+func (e *EncBE) Write(p []byte) {
+	e.push(p)
+}
+
+// Calls a WriteTo on a WriterTo and updates N/Err state
+func (e *EncBE) To(w io.WriterTo) {
+	if e.Err != nil {
+		return
+	}
+	n, err := w.WriteTo(e.W)
+	e.N += n
+	if err != nil {
+		e.Err = err
+	}
+}
+
+// Calls Marshal on a BinaryMarshaler and udpates N/Err state
+func (e *EncBE) Marshal(m encoding.BinaryMarshaler) {
+	if e.Err != nil {
+		return
+	}
+	buf, err := m.MarshalBinary()
+	if err != nil {
+		e.Err = err
+		return
+	}
+	e.push(buf)
+}
